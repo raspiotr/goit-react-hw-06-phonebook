e7dbac04f710db7abd8caed4404e314df8263021
raspiotr <raspiotr@gmail.com>
2023-11-27 14:35:05 +0100

    create redux files

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,43 +1,21 @@
-import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addContact } from 'redux/contactsSlice';
 import css from './ContactForm.module.css';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 
-export const ContactForm = props => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-
+export const ContactForm = () => {
+  const dispatch = useDispatch();
   const nameInputId = nanoid();
   const phoneInputId = nanoid();
 
-  const reset = () => {
-    setName('');
-    setNumber('');
-  };
-
   const handleSubmit = event => {
     event.preventDefault();
-    props.onSubmitHandle({
-      name: name,
-      number: number,
-    });
-    reset();
-  };
-
-  const handleChange = event => {
-    const { name, value } = event.target;
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
-
-      case 'number':
-        setNumber(value);
-        break;
-
-      default:
-        return;
-    }
+    const form = event.target;
+    const name = form.elements.name.value;
+    const number = form.elements.number.value;
+    dispatch(addContact({ name, number }));
+    form.reset();
   };
 
   return (
@@ -51,8 +29,6 @@ export const ContactForm = props => {
         pattern="[a-zA-Z\s'\-]*"
         name="name"
         id={nameInputId}
-        onChange={handleChange}
-        value={name}
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
       />
@@ -64,8 +40,6 @@ export const ContactForm = props => {
         type="tel"
         name="number"
         id={phoneInputId}
-        onChange={handleChange}
-        value={number}
         pattern="[0-9\s\-]+"
         title="Phone number must be digits and can contain spaces and dashes"
         required
---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const contactsInitialState = [];
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(inputData) {
+        return {
+          payload: {
+            id: nanoid(),
+            name: inputData.name,
+            number: inputData.number,
+          },
+        };
+      },
+    },
+    deleteContact(state, action) {
+      const index = state.findIndex(contact => contact.id === action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
